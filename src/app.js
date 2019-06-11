ca14bf465485328f2758b942c5fbfd98855eeb1a
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-11 08:54:58 +0300

    the client is now able to connect to the server, using socket.IO

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express'),
       path = require('path'),
-      http = require('http');
+      http = require('http'),
+      socketIo = require('socket.io');
 const app = express(),
-      server = http.createServer(app);
+      server = http.createServer(app),
+      io =socketIo(server);// <-- this is why we created the server - to have it support web sockets
+
+io.on('connection',()=>{
+  console.log('New web socket connection!!')
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
